8b9b94f88da7f44a08459da4782b982e40f14f05
Preeti yadav <preetiyadav30430@gmail.com>
2024-09-11 12:53:13 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from'mongoose';
 import dotenv from 'dotenv';
 import userrouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose
@@ -15,9 +16,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is runing on port 3000!');
 });
 
 
 app.use("/api/user" ,userrouter)
+app.use('/api/auth',authRouter)
